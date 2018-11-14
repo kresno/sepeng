271c655d341c51c43da2a5b501aeec9af96060be
kresno <kresno.19@gmail.com>
2018-11-14 08:07:01 +0700

    edit file-3

--- frontend/index.php
+++ frontend/index.php
@@ -65,18 +65,18 @@
                                 <li>
                                     <div id="sparklinedash2"></div>
                                 </li>
-                                <li class="text-right"><i class="ti-arrow-up text-purple"></i> <span class="counter text-purple">0</span></li>
+                                <li class="text-right"><i class="ti-arrow-up text-purple"></i> <span class="counter text-purple">2</span></li>
                             </ul>
                         </div>
                     </div>
                     <div class="col-lg-4 col-sm-6 col-xs-12">
                         <div class="white-box analytics-info">
-                            <h3 class="box-title">Total .....</h3>
+                            <h3 class="box-title">Total Indikator</h3>
                             <ul class="list-inline two-part">
                                 <li>
                                     <div id="sparklinedash3"></div>
                                 </li>
-                                <li class="text-right"><i class="ti-arrow-up text-info"></i> <span class="counter text-info">0</span></li>
+                                <li class="text-right"><i class="ti-arrow-up text-info"></i> <span class="counter text-info">3</span></li>
                             </ul>
                         </div>
                     </div>
