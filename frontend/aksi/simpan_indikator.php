17fd19472d60cd7b1b49f449b5db27cb4ed1e7e5
kresno <kresno.19@gmail.com>
2018-11-14 07:57:41 +0700

    edit file-1

--- frontend/aksi/simpan_indikator.php
+++ frontend/aksi/simpan_indikator.php
@@ -19,7 +19,7 @@ $renam = $_POST['renam'];
 $ktujuh = $_POST['ktujuh'];
 $rtujuh = $_POST['rtujuh'];
 
-$sql = "INSERT INTO indikator_kegiatan(kegiatan_id, satuan, ksatu, rsatu, kdua, rdua, ktiga, rtiga, kempat, rempat, klima, rlima, kenam, renam, ktujuh, rtujuh) VALUES ('$kegiatan_id', '$satuan', '$ksatu', '$rsatu', '$kdua', '$rdua', '$ktiga', '$rtiga', '$kempat', '$rempat', '$klima','$rlima', '$kenam', '$renam', '$ktujuh', '$rtujuh')";
+$sql = "INSERT INTO indikator_kegiatan(kegiatan_id, tolak_ukur, satuan, ksatu, rsatu, kdua, rdua, ktiga, rtiga, kempat, rempat, klima, rlima, kenam, renam, ktujuh, rtujuh) VALUES ('$kegiatan_id', '$tolak_ukur', '$satuan', '$ksatu', '$rsatu', '$kdua', '$rdua', '$ktiga', '$rtiga', '$kempat', '$rempat', '$klima','$rlima', '$kenam', '$renam', '$ktujuh', '$rtujuh')";
 if (!mysqli_query($con,$sql)) {
     die('Error: ' . mysqli_error($con));
 }else{
